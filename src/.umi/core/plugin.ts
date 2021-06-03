b4bc125e1b9b091be5481b57a053a95e5b5420de
Hui Cao <caohuiqaz@gmail.com>
2021-06-04 09:20:53 +1000

    first commit

---
+++ src/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from '/Users/macbookpro/it/antd-course/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','getInitialState','initialStateConfig','request',],
+});
+
+export { plugin };
---
+++ src/.umi/plugin-helmet/exports.ts
@@ -0,0 +1,3 @@
+// @ts-nocheck
+// @ts-ignore
+export { Helmet } from '/Users/macbookpro/it/antd-course/node_modules/react-helmet';
